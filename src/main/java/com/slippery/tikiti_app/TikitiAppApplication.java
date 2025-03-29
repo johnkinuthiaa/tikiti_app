d4b719b3afefa1ce609c943a1b979bee7feb8e5c
johnkinuthiaa <kinuthiajohn122@gmail.com>
2025-03-29 21:02:48 +0300

    initial commit

---
+++ src/main/java/com/slippery/tikiti_app/TikitiAppApplication.java
@@ -0,0 +1,13 @@
+package com.slippery.tikiti_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TikitiAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TikitiAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/slippery/tikiti_app/TikitiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slippery.tikiti_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TikitiAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
